e1b0c7238ad012df1f534db427714dd6b69edda8
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-01 18:35:58 +0900

    変数を使って計算

---
+++ php/lesson.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+
+
+
+  <?php
+
+    $x = 5;
+    $y = 2;
+    $a = 8;
+    $b = 4;
+    
+  ?>
+
+   <!-- ・$xの値に10を足し、$xを上書きしてください。
+・$xをechoしてください。 -->
+  <?php
+    $x += 10;
+    $x;
+    
+  ?>
+
+  <br>
+
+  <!-- ・$yに5をかけて、$yを上書きしてください。
+・echoを用いて$yを出力してください。 -->
+  <?php
+    $y *= 5;
+    $y;
+    
+  ?>
+
+  <br>
+
+  <!-- ・$aに1を足し、$aを上書きしてください。
+・echoを用いて$aを出力してください。 -->
+  <?php
+    $a++;
+    $a;
+    
+  ?>
+
+  <br>
+  
+  <!-- ・$bから1を引き、$bを上書きしてください。
+・echoを用いて$bを出力してください。 -->
+  <?php
+    $b--;
+    $b;
+    
+  ?>
+
+</body>
+</html>
