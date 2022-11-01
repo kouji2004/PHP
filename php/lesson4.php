6d41c7f5930df196cb43fa7d5d359b636e11e9e6
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-02 01:00:08 +0900

    条件分岐　else elseif文

---
+++ php/lesson4.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+    // $ageという変数に自分の年齢を代入してください
+    $age = 19;
+    if(30<=$age){
+      echo "あなたは30歳以上です。";
+    }else{
+      echo "あなたは30歳未満です。";
+    }
+    
+    
+  ?>
+
+</body>
+</html>
