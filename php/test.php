866878579f84b720817fffcdf1e7646c4eba8ebb
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-01 17:11:07 +0900

    PHP 環境構築

--- php/test.php
+++ php/test.php
@@ -1,1 +1,25 @@
-テスト
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Pろ</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <!-- この下に変数$fruitを書いていきましょう！ -->
+  <?php
+    $fruit = "りんご";
+     echo $fruit;
+  ?>
+
+  <br>
+
+  <!-- この下に変数$sumを書いていきましょう！ -->
+  <?php
+    $sum = 8 + 9;
+   echo $sum;
+  ?>
+
+</body>
+</html>
