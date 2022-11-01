85f50bfa71780234d701f8ab172bfd8f10203cb4
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-02 00:52:57 +0900

    条件分岐　if文

---
+++ php/lesson3.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+
+    $x = 99 * 99;
+    $y = 77 * 77;
+
+    // ここにif文を書いていきましょう
+    
+    //変数xは9800より大きいです。
+    if($x>9800){
+      echo "変数xは9800より大きいです。";
+    }
+    
+   // 変数yは6000より大きいです。
+    if($y>6000){
+      echo "変数yは6000より大きいです。";
+    }
+  ?>
+
+</body>
+</html>
