a019c2dd2142ece6920f50cf06fa8262eae899ad
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-02 12:13:16 +0900

    for文

---
+++ php/lesson9.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+for文を用いて、51から100までの数字をechoしてください。
+
+※echoするときに改行も入れるようにしてください。
+
+ヒント(改行)
+echo $i.'<br>';
+もしくは
+echo $i;
+echo '<br>';
+とすることで改行できます。
+
+
+
+
+  <?php
+
+    // この下にfor文を書いてください
+    for($i=51;$i<=100;$i++){
+      echo $i;
+      echo"<br>";
+    }
+    
+  ?>
+
+</body>
+</html>
