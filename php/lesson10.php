78e37df582fb943ecebc6f977015dd1b35b2c59b
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-02 12:21:57 +0900

    while文 while($i<=100)

---
+++ php/lesson10.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+問題
+変数$iを定義して、数値の2を代入してください。
+while文と変数$iを用いて、2から100までの偶数をechoしてください。
+
+※echoするときに改行も入れるようにしてください。
+
+
+
+
+  <?php
+
+    // 変数$iを定義してください
+    $i = 2;
+    
+    // while文を書いてください
+    while($i<=100){
+      echo $i."<br>";
+      $i+=2;
+    }
+    
+  ?>
+
+</body>
+</html>
+
