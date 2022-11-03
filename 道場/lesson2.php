1dd444e3b767f4578e813d1c751ecaa508d68a43
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-11-03 10:37:10 +0900

    道場 文字列の連結

---
+++ 道場/lesson2.php
@@ -0,0 +1,28 @@
+<?php
+$name = 'Tom';
+echo '変数$nameの値: '.$name;
+echo '<br>';
+echo '-----';
+echo '<br>';
+
+// 変数を用いて、名前を出力してみましょう。
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//解答
+echo "My name is".$name;
+
+?>
